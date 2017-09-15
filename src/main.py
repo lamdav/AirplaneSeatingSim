590b76282a4d0782f6f1ce31a8780339ba34bf9b
David Lam <dav_lam@ymail.com>
2017-09-14 20:07:50 -0400

    improved cli parser

--- src/main.py
+++ src/main.py
@@ -11,20 +11,19 @@ from src.ModelRunner import ModelRunner
 def main():
     parser = argparse.ArgumentParser(description="Simulate Plane Boarding")
 
-    parser.add_argument("--rows", "-r", help="Number of rows on the plane", default=3, nargs=1, type=int)
-    parser.add_argument("--seats", "-s", help="Number of seats on each side of the aisle of the plane", default=3, nargs=1, type=int)
+    parser.add_argument("--rows", "-r", help="Number of rows on the plane", default=3, type=int)
     parser.add_argument("--visualize", "-v", help="Display Visualizer", default=False, action="store_true")
     parser.add_argument("--model", "-m", help="Model to use", default="zig", choices=["zig", "zone"], type=str)
 
     args = parser.parse_args()
 
-    if (args.model == "zip"):
+    if (args.model == "zig"):
         model = ZigModel()
     else:
         model = ZoneModel()
 
     runner = ModelRunner()
-    runner.run(model, args.rows, args.seats, args.visualize)
+    runner.run(model, args.rows, 3, args.visualize)
 
 if __name__ == '__main__':
     main()
